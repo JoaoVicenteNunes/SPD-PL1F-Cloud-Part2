d32d6ed52fb208f3fb38e2a9053a4ae922a2435a
João Nunes <clasher00@gmail.com>
2022-06-04 16:19:36 +0000

    Still trying to fix static for Azure

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os.path
+from os import path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,8 +144,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'static/'
+PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__))) 
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\','/')  
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
